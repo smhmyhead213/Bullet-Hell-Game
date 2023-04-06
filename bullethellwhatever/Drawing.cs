f428f17f929ff0f14ce8febe98b3b101a0816526
smhmyhead213 <92687360+smhmyhead213@users.noreply.github.com>
2023-04-06 15:32:34 +0100

    Update Drawing.cs

--- bullethellwhatever/Drawing.cs
+++ bullethellwhatever/Drawing.cs
@@ -21,53 +21,65 @@ namespace bullethellwhatever
         {
             Main._spriteBatch.Begin();
 
-            if (Main.activeNPCs.Count == 0)
+            if (Main.activeNPCs.Count == 0) // stuff to draw while the player is not in combat 
             {
                 Utilities.drawTextInDrawMethod("Press Q to restart the fight. If you wish to change your settings or the difficulty, click the button.", new Vector2(Main._graphics.PreferredBackBufferWidth / 2, Main._graphics.PreferredBackBufferHeight / 2), Main._spriteBatch, Main.font, Color.White);
+                
+                //Add in the title screen button. This uses TitleScreenButton as its attributes fit what I need.
                 TitleScreenButton backToTitle = new TitleScreenButton(new Vector2(Main._graphics.PreferredBackBufferWidth / 2, Main._graphics.PreferredBackBufferHeight / 4 * 3), Main.startButton,
                 GameState.GameStates.TitleScreen, new Vector2(3, 3));
-
+                
+                //Only add the button to the list of active buttons if it has not yet been added to prevent it being added infinitely.
                 if (Main.activeButtons.Count < 1)
                     Main.activeButtons.Add(backToTitle);
-
+                
+                //Draw the button.
                 Main._spriteBatch.Draw(backToTitle.Texture, backToTitle.Position, null, Color.White, 0f, new Vector2(backToTitle.Texture.Width / 2, backToTitle.Texture.Height / 2), backToTitle.Scale, SpriteEffects.None, 0f);
 
 
 
             }
-
+            
+            //Debug text (to be removed)
             Utilities.drawTextInDrawMethod(Main.activeButtons.Count.ToString(), new Vector2(Main._graphics.PreferredBackBufferWidth / 5, Main._graphics.PreferredBackBufferHeight / 5), Main._spriteBatch, Main.font, Color.White);
 
-
+            //Calculate transparency based on the player's remaining immunity frames.
             float transparency = 4f * (1f / (Main.player.IFrames + 1f)); //to indicate iframes
-
+            
+            //Draw the player, accounting for immunity frame transparency.
             Main._spriteBatch.Draw(Main.player.Texture, Main.player.Position, null, Color.White * transparency, Main.player.Rotation, new Vector2(Main.player.Texture.Width / 2, Main.player.Texture.Height / 2), new Vector2(1, 1), SpriteEffects.None, 0f);
 
-
+            //Draw every active NPC.
             foreach (NPC npc in Main.activeNPCs)
             {
                 Main._spriteBatch.Draw(Main.player.Texture, npc.Position, null, npc.Colour(), npc.Rotation, new Vector2(Main.player.Texture.Width / 2, Main.player.Texture.Height / 2), npc.Size, SpriteEffects.None, 0f);
             }
-
+            
+            //Draw every enemy projectile.
             foreach (Projectile projectile in Main.activeProjectiles)
             {
                 Main._spriteBatch.Draw(Main.player.Texture, projectile.Position, null, projectile.Colour(), projectile.Rotation, new Vector2(Main.player.Texture.Width / 2, Main.player.Texture.Height / 2), projectile.Size, SpriteEffects.None, 0f);
             }
-
+            
+            //Draw every player projectile.
             foreach (Projectile projectile in Main.activeFriendlyProjectiles)
             {
                 Main._spriteBatch.Draw(Main.player.Texture, projectile.Position, null, projectile.Colour(), projectile.Rotation, new Vector2(Main.player.Texture.Width / 2, Main.player.Texture.Height / 2), projectile.Size, SpriteEffects.None, 0f);
             }
-
+            
+            //Draw the boss health bar. Note that active bosses will always be the first entries in the ActiveNPCs list.
             if (Main.activeNPCs.Count > 0)
             {
                 DrawHealthBar(Main._spriteBatch, Main.activeNPCs[0], new Vector2(Main._graphics.PreferredBackBufferWidth / 2, Main._graphics.PreferredBackBufferHeight / 20 * 19), 120f, 3f);
             }
-
+            
+            //Draw the player's health bar.
             DrawHealthBar(Main._spriteBatch, Main.player, new Vector2(Main._graphics.PreferredBackBufferWidth / 2, Main._graphics.PreferredBackBufferHeight / 20), 30f, 3f);
-
+            
+            //Select a control indicator based on the currently selected control scheme.
             string ControlInstruction = GameState.WeaponSwitchControl ? " , use the scroll wheel to switch weapons." : " , use the number keys 1, 2 and 3 to switch weapons";
 
+            //Write different text depending on which weapon is active
             switch (Main.player.ActiveWeapon)
             {
                 case Player.Weapons.Sharpshooter:
