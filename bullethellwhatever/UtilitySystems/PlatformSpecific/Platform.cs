92dc3cecec4f472647f84b760498ee3c067f10f9
smh my head <smhmyhead213@gmail.com>
2024-08-26 18:58:51 +0100

    commit to change application type

---
+++ bullethellwhatever/UtilitySystems/PlatformSpecific/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bullethellwhatever.UtilitySystems.PlatformSpecific
+{
+    public abstract class Platform
+    {
+        public abstract string SavePath
+        {
+            get;
+            set;
+        }
+        public abstract void OpenURL();
+
+
+    }
+}
