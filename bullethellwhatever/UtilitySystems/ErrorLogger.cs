c5fc9a5c37c1840c28b88d2896b2d16944ac1a01
smhmyhead <smhmyhead213@gmail.com>
2024-09-08 19:31:00 +0100

    wrote 3 lines of logger code

--- bullethellwhatever/UtilitySystems/ErrorLogger.cs
+++ bullethellwhatever/UtilitySystems/ErrorLogger.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using bullethellwhatever.MainFiles;
+using log4net;
+using log4net.Config;
 
 namespace bullethellwhatever.UtilitySystems
 {
-    public class ErrorLogger
+    public static class ErrorLogger
     {
+        private static ILog Log;
+
+        public static void Initialise()
+        {
+            Log = LogManager.GetLogger(typeof(Main));
+            BasicConfigurator.Configure();
+        }
     }
 }
