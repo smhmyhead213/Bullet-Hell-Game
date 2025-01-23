82e09f51d0f0242a79e8af9a9cb8e7231dfa6de7
smhmyhead <smhmyhead213@gmail.com>
2025-01-23 18:36:57 +0000

    moved crab IK test out

---
+++ bullethellwhatever/Bosses/CrabBoss/Attacks/CrabIKTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bullethellwhatever.UtilitySystems;
+using bullethellwhatever.Projectiles;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+using Microsoft.Xna.Framework.Graphics;
+using bullethellwhatever.DrawCode;
+using Microsoft.VisualBasic.Logging;
+
+namespace bullethellwhatever.Bosses.CrabBoss.Attacks
+{
+    public class CrabIKTest : CrabBossAttack
+    {
+
+        public CrabIKTest(CrabBoss owner) : base(owner)
+        {
+
+        }
+
+        public override void Execute(int AITimer)
+        {
+            foreach (CrabLeg leg in CrabOwner.Legs)
+            {
+                Vector2 targetPosition = leg.Position + 1f * (MousePositionWithCamera() - leg.Position); // change this to make bend testing easier
+                leg.TouchPoint(targetPosition);
+            }
+        }
+
+        public override BossAttack PickNextAttack()
+        {
+            return new CrabFlail(CrabOwner, 0);
+        }
+
+        public override void ExtraDraw(SpriteBatch s)
+        {
+
+        }
+    }
+}
