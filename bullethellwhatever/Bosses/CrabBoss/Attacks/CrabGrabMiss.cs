9654c3e5eb0f1b479431e606ff5353118e9ac7fe
smhmyhead <smhmyhead213@gmail.com>
2025-03-14 12:55:08 +0000

    more work on shockwave scream

--- bullethellwhatever/Bosses/CrabBoss/Attacks/CrabGrabMiss.cs
+++ bullethellwhatever/Bosses/CrabBoss/Attacks/CrabGrabMiss.cs
@@ -35,7 +35,8 @@ namespace bullethellwhatever.Bosses.CrabBoss.Attacks
 
                 if (AITimer % screamPeriod == 0)
                 {
-                    ShockwaveRing shockwave = new ShockwaveRing(0f, 100f, 8, 3);
+                    Drawing.ScreenShake(7, screamPeriod);
+                    ShockwaveRing shockwave = new ShockwaveRing(0f, 130f, 4, 2);
                     shockwave.ScrollSpeed = 0.04f;
                     shockwave.Spawn(Owner.Position, Owner, Color.Gray);
                 }
