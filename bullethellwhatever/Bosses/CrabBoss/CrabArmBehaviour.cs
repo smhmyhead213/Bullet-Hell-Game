f2d5152b550d39097d465687c21eff1ff9958e7a
smhmyhead <smhmyhead213@gmail.com>
2025-10-13 12:41:18 +0100

    began implementation of separate arm behaviours

---
+++ bullethellwhatever/Bosses/CrabBoss/CrabArmBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bullethellwhatever.Bosses.CrabBoss
+{
+    public abstract class CrabArmBehaviour
+    {
+        public CrabBoss Owner;
+        public int ArmIndex;
+        public int AITimer;
+        public CrabArmBehaviour(CrabBoss owner, int armIndex)
+        {
+            Owner = owner;
+            ArmIndex = armIndex;
+        }
+
+        public abstract void Execute();
+        public CrabArm Arm()
+        {
+            return Owner.Arms[ArmIndex];
+        }
+    }
+}
